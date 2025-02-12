b2b9f2f7cd34e3536082a4bcaaaba058d5548c86
itajacy TI02 <itajacy@bsd.com.br>
2025-02-12 16:29:09 -0300

    base configurada

---
+++ lib/animation3.dart
@@ -0,0 +1,11 @@
+import 'package:animacao3/home_page.dart';
+import 'package:flutter/material.dart';
+
+class Animation3 extends StatelessWidget {
+  const Animation3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Animação Controlada 3', home: HomePage());
+  }
+}
